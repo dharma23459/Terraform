0193d0d7a01cbe5fab84f7f1536d5555244000b6
Dharma <dharmannn635@gmail.com>
2025-02-20 16:51:18 +0530

    day8

---
+++ Day-2-code-for-module/variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+    description = "inserting ami value"
+    type = string
+    default = ""
+}
+
+variable "type" {
+    description = "instance type"
+    type = string
+    default = ""
+}
+
+variable "key" {
+    description = "inserting key value"
+    type = string
+    default = ""
+}
---
+++ Day-8-Modules/module.tf
@@ -0,0 +1,6 @@
+module "dharma" {
+    source = "../Day-2-code-for-module"
+    ami_id = "ami-0ddfba243cbee3768"
+    type = "t2.micro"
+    key = "sainikudu"
+}
