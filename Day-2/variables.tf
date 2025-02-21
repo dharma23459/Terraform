cb334da1446624608454590e5fd7229006fa9afd
Dharma <dharmannn635@gmail.com>
2025-02-21 23:27:12 +0530

    day-9

--- Day-2/variables.tf
+++ Day-2/variables.tf
@@ -1,17 +1,17 @@
 variable "ami_id" {
     description = "inserting ami value"
     type = string
-    default = "ami-0ddfba243cbee3768"
+    default = ""
 }
 
 variable "type" {
     description = "instance type"
     type = string
-    default = "t2.micro"
+    default = ""
 }
 
 variable "key" {
     description = "inserting key value"
     type = string
-    default = "sainikudu"
+    default = ""
 }
